5a8e646f199f120249a3606da597259876b75a07
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-09-27 17:03:24 +0500

    swap backend url

--- axios.config.js
+++ axios.config.js
@@ -1,22 +1,22 @@
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: "http://5.101.50.171/api/",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default axiosInstance;
-
 import axios from "axios";
 
-// Создание экземпляра Axios с настройками
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "http://80.249.144.137/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default axiosInstance;
+
+// import axios from "axios";
+
+// // Создание экземпляра Axios с настройками
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:8080/api/",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default axiosInstance;
