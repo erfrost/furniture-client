c090772a23ebc8562e8f0148d1c35b501dd74944
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-10-10 22:16:49 +0500

    fix: backend url

--- axios.config.js
+++ axios.config.js
@@ -1,19 +1,8 @@
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: "http://80.249.144.137:8080/api/",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default axiosInstance;
-
 import axios from "axios";
 
 // Создание экземпляра Axios с настройками
 const axiosInstance = axios.create({
-  baseURL: "http://31.184.254.214:8080/api/",
+  baseURL: "https://api.dom888.ru/api/",
   headers: {
     "Content-Type": "application/json",
   },
