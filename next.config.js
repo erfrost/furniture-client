673fcce949902435e59959d43a94b28afd14169e
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-10-15 21:14:54 +0500

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
-// const runtimeCaching = require("next-pwa/cache");
-// const withPWA = require("next-pwa")({
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//     runtimeCaching,
-//   },
-// });
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+});
 
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ["https://api.admin"],
   },
-};
-
-module.exports = nextConfig;
+});
