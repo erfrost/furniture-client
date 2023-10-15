622e5cae8e870cf1de9aa3c3f0387e4ec331b83f
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-10-15 16:57:19 +0500

    debug

--- next.config.js
+++ next.config.js
@@ -1,31 +1,21 @@
-// const withPWA = require("next-pwa");
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: [
-//       {
-//         protocol: "https",
-//         domain: "**",
-//       },
-//     ],
-//   },
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//   },
-// };
-
-// module.exports = withPWA(nextConfig);
-
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      {
+        protocol: "https",
+        domain: "**",
+      },
+    ],
+  },
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
   },
-});
+};
+
+module.exports = withPWA(nextConfig);
