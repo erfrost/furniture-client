f83f83ae046837800fa8e0e25e032756ad264a7e
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-12-19 17:34:38 +0500

    fix: yandex metrika

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import Script from "next/script";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -55,32 +56,35 @@ export default function App({ Component, pageProps }) {
           name="google-site-verification"
           content="1Bvp_6TjYTPGy2u5RoIMW-fzh547OhfeZqyppeg6MKE"
         />
-        <script></script>
       </Head>
       <RecoilRoot>
         <ChakraProvider>
           <Component {...pageProps} />
           <Script src="//code.jivo.ru/widget/78D5IbCyIB" async></Script>
-          <Script id="Yandex.Metrika">
-            {`<!-- Yandex.Metrika counter -->
-            <script type="text/javascript" >
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          <script type="text/javascript">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-              ym(95928345, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true,
-                    ecommerce:"dataLayer"
-              });
-            </script>
-            <noscript><div><img src="https://mc.yandex.ru/watch/95928345" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-            <!-- /Yandex.Metrika counter -->`}
-          </Script>
+   ym(95928345, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+   })`}
+          </script>
+          <noscript>
+            <div>
+              <Image
+                src="https://mc.yandex.ru/watch/95928345"
+                style="position:absolute; left:-9999px;"
+                alt=""
+              />
+            </div>
+          </noscript>
         </ChakraProvider>
       </RecoilRoot>
     </>
